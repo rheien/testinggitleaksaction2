41cd85f7c04df8a2d5245ca300d16573c594597b
Ryan <90624129+rheien@users.noreply.github.com>
2021-12-03 09:14:39 +0100

    Update puppy.py

--- puppy.py
+++ puppy.py
@@ -1,3 +1,47 @@
 admin-12345
 
 admin-asdfgj
+
+{
+“token”: {
+	“url”: “https://emplae……..”,
+	“id”: 12345678,
+	“user_id”: 1234567890,
+	“client_id”: 23456,
+	“refresh_token”: null,
+	“created_at”:	“20xx-xx-xxThh:mm:ssz”,
+	“expires_at”:	:null,
+	“used_at”: null,
+	}
+}
+“token”: “d3d26efa9”
+
+AKIA1234567890ASDFGH
+
+{
+“token”: {
+	“url”: “https://emplae……..”,
+	“id”: 12345678,
+	“user_id”: 1234567890,
+	“client_id”: 23456,
+	“refresh_token”: null,
+	“created_at”:	“20xx-xx-xxThh:mm:ssz”,
+	“expires_at”:	:null,
+	“used_at”: null,
+	}
+}
+“token”: “d3d26efa9”
+  
+  {
+“token”: {
+	“url”: “https://emplae……..”,
+	“id”: 12345678,
+	“user_id”: 1234567890,
+	“client_id”: 23456,
+	“refresh_token”: null,
+	“created_at”:	“20xx-xx-xxThh:mm:ssz”,
+	“expires_at”:	:null,
+	“used_at”: null,
+	}
+}
+“token”: “d3d26efa9”
